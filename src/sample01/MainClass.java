9fed06fda3e161fd4c89509d231ea8a8c24d23af
jyeonshin <skyblue6393@naver.com>
2023-01-06 14:32:35 +0900

    workproject

---
+++ src/sample01/MainClass.java
@@ -0,0 +1,49 @@
+package sample01;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("��ο� �ڹ�");
+		
+		// �� �� �ּ��� -> �������� ���� ����
+		
+		/*
+		 	���� �ּ��� 
+		 	
+		 	Html        <!-- -->
+
+		 	JSP         <%-- --%>
+		 	
+		 	mySQL       --
+		 	
+		 	python      #   """ �ּ��� """
+		 	
+		 */
+		
+		System.out.println("Hello World~!");
+		
+		System.out.print("Hello");
+		
+		// ��� : escape sequence
+		//       \n(����)
+		
+		System.out.print("World\n");
+		
+		System.out.println("��ο�\b");
+		
+		System.out.println("\"Hello World\"");
+		
+		System.out.println("\tHello\tWorld");
+		System.out.println("\t헬로우\t월드");
+		
+		System.out.println("\\��ο� ���� \\");
+		
+		System.out.printf("%s", "Hello");
+		
+		System.out.println(  1024  );
+		
+		System.out.println("Hello" + "World");
+	}
+
+}
